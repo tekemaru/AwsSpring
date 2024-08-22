2a7f41eff982fc82ae6c5ea8d9ce3ab3889efaea
tekemaru <dpm10198@gmail.com>
2024-08-22 18:00:41 +0900

    My First Commit

---
+++ src/main/java/com/example/simpleapplication/SimpleSpringApllicationApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpleapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringApllicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringApllicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/simpleapplication/SimpleSpringApllicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpleapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringApllicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
